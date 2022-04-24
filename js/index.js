faa8501b9056d2b6c248ea1facbb9125125d0394
root <root@kali>
2022-04-24 18:21:42 -0400

    BACKEND teilweise

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//Das ist die Datei mit meinem Servercode
+//package-lock file juckt uns nicht 
+
+const express = require("express"); //import Express
+const app = express(); //ganze Library in einer Variable
+
+//listening
+app.listen(3000, ()=>console.log("hört auf port 3000"));
+//danach muss ich node index.js ausführen damit das läuft
+
+//Nutzer angegebenen Ordner wenn Client sich verbindet
+app.use(express.static("OrdnerFuerClients"))
+//
